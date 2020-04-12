73fe1f4798e72d2add0c343f1ea6b6a165c94574
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-04-12 04:09:15 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+function superbowWin(record) {
+  let sadReality;
+  record.find(function (line) {
+    if (line.result === "W") {
+      return line.year
+    } else {
+      sadReality = undefined;
+      return sadReality;
+    }
+  })
+}
+superbowWin(record)
+
