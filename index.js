873ba6b51b75f539f4d14a925a6801d12c550401
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-04-12 04:13:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const testVar = {}
+// const testVar = {}
 
-function testFunc() {
-  return "hi"
-}
+// function testFunc() {
+//   return "hi"
+// }
 
-const record = [
-  {year: "2018", result: "L"},
-  {year: "2017", result: "W"},
-  {year: "2016", result: "N/A"}
-  //...
-]
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "W"},
+//   {year: "2016", result: "N/A"}
+//   //...
+// ]
 
 superbowWin = (record) => {
   let result = record.find(line => line.result === "W")
